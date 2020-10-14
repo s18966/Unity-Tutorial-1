0d48382ae5cae262eee630ba1ce653d761dfbbbc
s18966 <s18966@LAB001-11>
2020-10-14 18:57:33 +0200

    first com

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 50;
+    public float jumpForce = 500;
+    //public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+        } 
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
